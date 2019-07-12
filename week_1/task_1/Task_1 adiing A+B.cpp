7d8a3fc01169b9010b65b674b77fe955744411f8
ypostolov <ypostolov@luxoft.com>
2019-07-12 17:56:59 +0300

    fixup! 1st week, 1st task

--- week_1/task_1/Task_1 adiing A+B.cpp
+++ week_1/task_1/Task_1 adiing A+B.cpp
@@ -6,6 +6,6 @@ int main()
   int a,b;
   std::cout << "Please enter two integer numbers ";
   std::cin>>a>>b;
-  std::cout << "A + B = " << a+b <<std::endl;
+  std::cout << a+b <<std::endl;
   return 0;
 }
