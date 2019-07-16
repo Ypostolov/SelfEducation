46eb71710e74b62f965de58296442bfa9330b21c
ypostolov <ypostolov@luxoft.com>
2019-07-16 15:55:54 +0300

    1_week 2_task

---
+++ week_1/task_2/Task_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string string_1,
+              string_2,
+              string_3, 
+              min_string;
+  std::cin >> string_1>>string_2>>string_3;
+  
+  if (string_1 < string_2 && string_1 < string_3){
+      min_string = string_1;}
+      else if(string_2 < string_1 && string_2 < string_3){
+          min_string  = string_2;}
+          else {min_string =string_3;}
+  
+  std::cout <<min_string;
+ }
